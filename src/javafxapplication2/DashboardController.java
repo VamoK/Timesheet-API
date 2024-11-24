21fd88a5152d294bc688d274c69ef74757027fcd
Vamokuhle <vamokuhlekhumalo4@gmail.com>
2024-11-24 17:18:46 +0200

    Vamo

--- src/javafxapplication2/DashboardController.java
+++ src/javafxapplication2/DashboardController.java
@@ -4,9 +4,18 @@
  */
 package javafxapplication2;
 
+import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 /**
  * FXML Controller class
@@ -15,12 +24,42 @@ import javafx.fxml.Initializable;
  */
 public class DashboardController implements Initializable {
 
+    @FXML
+    private BorderPane bp;
+    
+    @FXML
+    private AnchorPane ap;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }  
     
+    private void loadPage(String page){
+        
+        Parent root = null;
+        
+        try {
+            
+            
+            root = FXMLLoader.load(getClass().getResource(page+".fxml"));
+        } catch (IOException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        bp.setCenter(root);
+        
+    }
+    @FXML
+    private void createTimesheet(MouseEvent event){
+        
+        bp.setCenter(ap);
+    }
+    
+    @FXML
+    private void viewTimesheet(MouseEvent event){
+         loadPage("view_timesheet");
+    }
 }
