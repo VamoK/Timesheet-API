02760dc67dcc0e034d50c2e47b2b3e2417365626
Vamokuhle <vamokuhlekhumalo4@gmail.com>
2024-11-25 15:01:18 +0200

    Vamo

--- src/javafxapplication2/DashboardController.java
+++ src/javafxapplication2/DashboardController.java
@@ -5,8 +5,17 @@
 package javafxapplication2;
 
 import java.net.URL;
+import java.time.LocalTime;
 import java.util.ResourceBundle;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -18,6 +27,42 @@ public class DashboardController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    private LocalTime startTime;
+    
+    private Timeline timeline;
+    
+    @FXML
+    private TextArea projectName;
+    
+    @FXML
+    private TextArea projectDec;
+    
+    @FXML
+    private Text display;
+    
+    @FXML
+    private LocalTime startCount(ActionEvent event){
+        
+        startTime = LocalTime.now();
+        
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), e -> updateElapsedTime()));
+        
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+        
+        return startTime;
+    }
+    
+    private void updateElapsedTime() {
+        LocalTime now = LocalTime.now();
+        long seconds = java.time.Duration.between(startTime, now).getSeconds();
+        long hours = seconds / 3600;
+        long minutes = (seconds % 3600) / 60;
+        seconds = seconds % 60;
+
+        display.setText(String.format("Elapsed Time: %02d:%02d:%02d", hours, minutes, seconds));
+        display.setVisible(true);
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
